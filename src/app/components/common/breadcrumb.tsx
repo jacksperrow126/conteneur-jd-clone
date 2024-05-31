57b99f20226dbb745bccb9fb29b873b9fd0ddcef
annguyenthanh126 <an.nguyen@missmp.eu>
2024-05-31 16:39:35 +0700

    feat: fix bug and update ui
    Signed-off-by: annguyenthanh126 <an.nguyen@missmp.eu>

--- src/app/components/common/breadcrumb.tsx
+++ src/app/components/common/breadcrumb.tsx
@@ -1,3 +1,5 @@
+import { useTranslations } from 'next-intl';
+
 const Breadcrumb = ({
   title,
   isShowContactButton,
@@ -5,17 +7,18 @@ const Breadcrumb = ({
   title: string;
   isShowContactButton?: boolean;
 }>) => {
+  const t = useTranslations('Common');
   return (
     <div
       className="bg-[url('https://conteneurjd.com/wp-content/uploads/2023/06/separator.jpg')] bg-no-repeat bg-cover bg-center
         h-[180px] w-full flex items-center"
       data-aos="fade-up"
     >
-      <div className="flex justify-between w-full">
-        <p className="text-[40px] text-white uppercase ml-12">{title}</p>
+      <div className="flex justify-between w-ful md:flex-row flex-col p-4">
+        <p className="text-[30px] text-white uppercase font-bold">{title}</p>
         {isShowContactButton && (
           <div className="bg-white flex justify-center items-center mr-12 px-8">
-            <button>Contact</button>
+            <button>{t('Contact')}</button>
           </div>
         )}
       </div>
--- src/app/components/common/footer.tsx
+++ src/app/components/common/footer.tsx
@@ -5,15 +5,25 @@ const Footer = () => {
   return (
     <footer>
       <div className="w-full bg-gray-200">
-        <div className="p-12">
+        <div className="rotate-180 -scale-x-100">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 2000 120"
+            preserveAspectRatio="none"
+          >
+            <polygon fill="#FFF" points="-11,2 693,112 2019,6 2019,135 -11,135 "></polygon>
+          </svg>
+        </div>
+        <div className="md:p-12 p-2">
           <div className=" mt-4 flex md:flex-row flex-col">
             <div className="flex-1 m-4">
               <h1 className=" text-[30px]">Company name</h1>
               <p className="text-white">{t('Slogan')}</p>
               <p className="text-gray-100">{t('Slogan')}</p>
-
               <p className="text-gray-100">{t('Slogan')}</p>
-
               <p className="text-gray-100">{t('Slogan')}</p>
             </div>
             <div className="flex-1 m-4">
@@ -26,9 +36,7 @@ const Footer = () => {
               <p className="text-gray-100 my-2">{t('Slogan')}</p>
               <p className="text-gray-100 my-2">{t('Slogan')}</p>
               <p className="text-gray-100 my-2">{t('Slogan')}</p>
-
               <p className="text-gray-100 my-2">{t('Slogan')}</p>
-
               <p className="text-gray-100 my-2">{t('Slogan')}</p>
             </div>
           </div>
