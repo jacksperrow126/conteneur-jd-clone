436482fd99d4a6cd3b7dd2fcf77808778bb5c63a
Nguyễn Văn Kiệt <vankiet.kn78@gmail.com>
2024-06-01 15:16:35 +0700

    2

--- src/app/components/common/breadcrumb.tsx
+++ src/app/components/common/breadcrumb.tsx
@@ -14,7 +14,12 @@ const Breadcrumb = ({
       <div className="flex justify-between w-full">
         <p className="text-[40px] text-white uppercase ml-12">{title}</p>
         {isShowContactButton && (
-          <div className="bg-white flex justify-center items-center mr-12 px-8">
+          <div
+            className=" border-white  flex justify-center items-center mr-12 px-8 bg-white text-black cursor-pointer hover:bg-primary hover:text-white"
+            style={{
+              clipPath: 'polygon(90% 0%, 100% 50%, 90% 100%, 0% 100%, 0% 0%)',
+            }}
+          >
             <button>Contact</button>
           </div>
         )}
