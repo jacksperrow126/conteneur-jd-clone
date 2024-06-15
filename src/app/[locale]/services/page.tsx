c889a29dc3de232c62af9b972807d39c27e0775e
Nguyễn Văn Kiệt <vankiet.kn78@gmail.com>
2024-06-15 17:01:16 +0700

    padding

--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -76,11 +76,11 @@ const Contact = () => {
           );
         })}
       </div>
-      <section className="flex  md:flex-row flex-col my-8">
+      <section className="flex  md:flex-row flex-col my-8 px-10">
         <div className="flex-1">
           <ContactInfo />
         </div>
-        <div className="flex-1">
+        <div className="flex-1w">
           <Form />
         </div>
       </section>
